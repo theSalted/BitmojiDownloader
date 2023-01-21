3d3965373284fac084919af49bb77bc07b58e18f
Yuhao <hello@chenyuhao.me>
2023-01-21 00:17:17 -0800

    Initial Commit

---
+++ Bitmoji Downloader/Bitmoji_DownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bitmoji_DownloaderApp.swift
+//  Bitmoji Downloader
+//
+//  Created by Yuhao Chen on 1/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Bitmoji_DownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
